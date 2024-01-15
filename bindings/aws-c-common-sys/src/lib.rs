9cff986eb9bcc3a422039127e221df1fad428f01
Simon Berger <simon.berger@inomotech.com>
2024-01-15 17:31:38 +0100

    make sure windows features are activated

--- bindings/aws-c-common-sys/src/lib.rs
+++ bindings/aws-c-common-sys/src/lib.rs
@@ -18,16 +18,26 @@ extern crate ittapi_sys;
 
 #[cfg(windows)]
 extern crate windows_sys;
+#[cfg(target_os = "android")]
+#[link(name = "log")]
+extern "C" {}
 
 #[cfg(target_vendor = "apple")]
 use core_foundation_sys::base::CFAllocatorRef;
 use libc::*;
 
-#[cfg(target_os = "android")]
-#[link(name = "log")]
-extern "C" {}
-
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 // needs to be done manually because it's defined in an unnamed enum
 pub const AWS_LOG_SUBJECT_STRIDE_BITS: u32 = 10;
+
+#[cfg(test)]
+mod tests {
+    /// Checks whether the necessary windows-sys features have been activated
+    #[cfg(windows)]
+    #[test]
+    fn win32_symbols() {
+        #![allow(unused_imports)]
+        use windows_sys::Win32::UI::Shell::PathFileExistsW as _;
+    }
+}
