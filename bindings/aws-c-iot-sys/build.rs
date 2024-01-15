846aece9ff14fd7b504da8104d4fb019ff246856
Simon Berger <simon.berger@inomotech.com>
2024-01-15 18:01:14 +0100

    build iot correctly :O

--- bindings/aws-c-iot-sys/build.rs
+++ bindings/aws-c-iot-sys/build.rs
@@ -1,6 +1,15 @@
 fn main() {
     let ctx = aws_c_builder::Context::new();
-    ctx.builder("aws-c-iot")
+    let mut builder = ctx.builder("aws-c-iot");
+    if ctx.is_win32() {
+        builder.source_path("windows");
+    } else if ctx.is_apple() {
+        builder.source_path("apple");
+    } else if ctx.is_unix() {
+        builder.source_path("linux");
+    }
+
+    builder
         .aws_set_common_properties()
         .dependencies([
             "aws-c-mqtt",
